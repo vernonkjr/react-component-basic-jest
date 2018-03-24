6adfc8a1d4cbafca2bae9352d49d228121f1d7a8
vernonkjr <vkerrjr@gmail.com>
2018-03-24 11:56:15 -0400

    add more App specs

--- src/App.test.js
+++ src/App.test.js
@@ -11,4 +11,36 @@ describe('App', () => {
       wrapper.contains(<th>Items</th>)
     ).toBe(true);
   });
+
+  it('should have a `button` element', () => {
+    const wrapper = shallow(
+      <App />
+    );
+    expect(
+      wrapper.containsMatchingElement(
+        <button>Add item</button>
+      )
+    ).toBe(true);
+  });
+
+  it('should have an `input` element', () => {
+    const wrapper = shallow(
+      <App />
+    );
+    expect(
+      wrapper.containsMatchingElement(
+        <input />
+      )
+    ).toBe(true);
+  });
+
+  it('`button` should be disabled', () => {
+    const wrapper = shallow(
+      <App />
+    );
+    const button = wrapper.find('button').first();
+    expect(
+      button.props().disabled
+    ).toBe(true);
+  });
 });
